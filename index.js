48299b2518f864716eabfd8e5d2fcf2cc6bbc1fa
henrique-HR <99933690+henrique-HR@users.noreply.github.com>
2022-12-18 13:53:19 -0300

    fim

---
+++ index.js
@@ -0,0 +1,19 @@
+const nao = document.querySelector('#nao')
+const sim = document.querySelector('#sim')
+
+const pedido = document.querySelector('main')
+
+
+nao.addEventListener("mouseenter",()=>{
+        const y = Math.floor(Math.random()*-90)
+        const x = Math.floor(Math.random()*-90)  
+            nao.style.top = `${-y}%`
+            nao.style.left = `${-x}%`
+            
+})
+sim.addEventListener('click',()=>{
+    pedido.innerHTML = 'favorite'
+    pedido.classList.add('material-symbols-outlined')
+    pedido.classList.add('pulsar')
+    
+})
